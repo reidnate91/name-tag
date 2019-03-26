707f5657952f1b92ac3e06c5b175f55601abcaa4
Nate Reid <reidnate1991@gmail.com>
2019-03-26 13:53:40 -0600

    finished assignment

--- src/nametag.js
+++
@@ -1,9 +0,0 @@
-<script type="text/javascript">
-function changeText2(){
-	const userInput = document.getElementById('userInput').value;
-	document.getElementById('demo').innerHTML = userInput;
-}
-</script>
-<p>Welcome to the site <b id='demo'>dude</b> </p> 
-<input type='text' id='userInput' value='Enter Text Here' />
-<input type='button' onclick='changeText2()' value='Change Text'/>
